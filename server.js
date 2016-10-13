106da9987f8e5b54d3cc906fc3416583a9183bc4
Trey Amador <tmamador@cpp.edu>
2016-10-12 20:25:43 -0700

    revised server with new morgan library

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 var app = express();
 
 app.set('view engine','ejs');
@@ -13,6 +14,13 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'node_modules')));
 app.use(bodyParser());
 app.use(require('./router'));
+
+// determines if logging is skipped
+logger('combined',{
+	skip: function(req, res){
+		return res.statusCode < 400;
+	}
+});
 
 var port = 8080;
 app.listen(port, function(){
